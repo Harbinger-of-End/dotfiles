feaa6c82dcbaa0322c752eb85eefd3e0d43612e4
Sai Kumar <ksai44576@gmail.com>
2023-04-12 19:08:48 +0530

    Created a basic setup for Neovim

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,71 @@
+return require('packer').startup(function(use)
+    -- Packer.nvim for managing plugins
+    use 'wbthomason/packer.nvim'
+    -- COC for autocompletion
+    use {
+        'neoclide/coc.nvim',
+        branch = 'release',
+        run = 'yarn install --frozen-lockfile',
+    }
+    -- Treesitter for parsing
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    -- Ayu theme for Neovim
+    use 'Shatur/neovim-ayu'
+    -- nvim-tree for file navigation
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = 'nvim-tree/nvim-web-devicons'
+    }
+    -- Telescope for fuzzy finder
+    use 'nvim-telescope/telescope.nvim'
+    -- Toggleterm for terminal
+    use 'akinsho/toggleterm.nvim'
+    -- Gitsigns for git integration
+    use {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup {
+                signs                        = {
+                    add          = { text = 'A' },
+                    change       = { text = 'C' },
+                    delete       = { text = 'D' },
+                    topdelete    = { text = 'TD' },
+                    changedelete = { text = 'CD' },
+                    untracked    = { text = 'U' },
+                },
+                signcolumn                   = true,
+                numhl                        = false,
+                linehl                       = false,
+                word_diff                    = false,
+                watch_gitdir                 = {
+                    interval = 1000,
+                    follow_files = true
+                },
+                attach_to_untracked          = true,
+                current_line_blame           = false,
+                current_line_blame_opts      = {
+                    virt_text = true,
+                    virt_text_pos = 'eol',
+                    delay = 1000,
+                    ignore_whitespace = false,
+                },
+                current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+                sign_priority                = 6,
+                update_debounce              = 100,
+                status_formatter             = nil,
+                max_file_length              = 40000,
+                preview_config               = {
+                    border = 'single',
+                    style = 'minimal',
+                    relative = 'cursor',
+                    row = 0,
+                    col = 1
+                },
+                yadm                         = {
+                    enable = false
+                },
+            }
+        end
+    }
+    use 'rebelot/heirline.nvim'
+end)
