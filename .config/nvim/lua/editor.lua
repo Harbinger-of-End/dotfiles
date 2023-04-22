e08e47391ce03fb3327142914b0e4b2cf47312a3
Sai Kumar <ksai44576@gmail.com>
2023-04-22 12:20:49 +0530

    Added some more editor configurations

--- .config/nvim/lua/editor.lua
+++ .config/nvim/lua/editor.lua
@@ -1,6 +1,12 @@
 vim.o.smarttabs = false
 vim.o.nu = true
+vim.o.encoding = 'utf-8'
+vim.o.nobackup = true
+vim.o.nowritebackup = true
+vim.o.updatetime = 300
 vim.o.relativenumber = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.g.editorconfig = true
+
+vim.g.mapleader = ' '
